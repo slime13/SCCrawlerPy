8800f65d84270fbc148b761a05dcd5f04889506b
Wonwoo Ro <wonwoo.ro2@gmail.com>
2020-11-01 22:39:24 +0900

    Update SCCrawler.py

--- SCCrawler.py
+++ SCCrawler.py
@@ -8,7 +8,6 @@ def crawl(endNumber = -1):
 	#DB 연결
 	try:
 		conn = psycopg2.connect(database="dsidb", user="postgres", password="developer#!", host="121.129.214.6", port="8432")
-		cur = conn.cursur()
 	except Exception as inst:
 		print(inst)
 		return 1
