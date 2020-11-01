2eb61918901c311edece300fe8ea2611e4b1767d
Wonwoo Ro <wonwoo.ro2@gmail.com>
2020-11-01 22:37:33 +0900

    db test init

--- SCCrawler.py
+++ SCCrawler.py
@@ -2,9 +2,19 @@ import requests
 import time
 from bs4 import BeautifulSoup as bs
 from SCPageParser import parsePage
+import psycopg2
 
-def crawl():
+def crawl(endNumber = -1):
+	#DB 연결
+	try:
+		conn = psycopg2.connect(database="dsidb", user="postgres", password="developer#!", host="121.129.214.6", port="8432")
+		cur = conn.cursur()
+	except Exception as inst:
+		print(inst)
+		return 1
+
 	pageNumber = 1
+
 	while(1):
 		#제안과제 리스트 파싱
 		target_pages = []
@@ -36,23 +46,30 @@ def crawl():
 				print(inst)
 				continue
 			try:
+				#f = open('dump/%s.txt' % (targetPage['REGION'] + '-' + targetPage['UID']), 'w', encoding='utf-8')
+				#for key in result:
+				#	f.write('%s: %s\n' % (key, result[key]))
+				#f.close()
+				#############DB에 입력##############
 				result = parsePage(page, url, targetPage['REGION'])
-				f = open('dump/%s.txt' % (targetPage['REGION'] + '-' + targetPage['UID']), 'w', encoding='utf-8')
-				for key in result:
-					f.write('%s: %s\n' % (key, result[key]))
-				f.close()
+				sql = "INSERT INTO crawling_sckr (%s, %s, %s, %s, %s, %s, '')" % (targetPage['UID'], result['location'], result['title'], result['date'], result['contents'], result['url'])
+				cur.execute(sql)
+				###################################
 			except Exception as inst:
 				print(inst)
 				print('FAIL TO PARSE PAGE #%s' % targetPage['UID'])
-
+		#endNumber가 정의 되어 있을 경우, endNumber에서 탐색 종료 
+		if pageNumber == endNumber:
+			break
 		#페이지 넘김
 		targetPage.clear()
 		pageNumber += 1
-	
+
+	conn.close()
 
 if __name__ == '__main__':
 	print('Start Module Testing for SCCrawler.', flush=True)
-	time.sleep(1)
-	crawl()
+	crawl(1)
+	print('Done.', flush=True)
 		
 		
--- postgresql_test.py
+++ postgresql_test.py
@@ -5,3 +5,5 @@ print('psycopg2 install test.')
 conn = psycopg2.connect(database="dsidb", user="postgres", password="developer#!", host="121.129.214.6", port="8432")
 
 print(conn)
+
+conn.close()
