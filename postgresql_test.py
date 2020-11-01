38b79f02f2d353aadcfe48040108d24b5348cc8f
Wonwoo Ro <wonwoo.ro2@gmail.com>
2020-11-01 22:16:05 +0900

    Update postgresql_test.py

--- postgresql_test.py
+++ postgresql_test.py
@@ -2,4 +2,6 @@ import psycopg2
 
 print('psycopg2 install test.')
 
-connection = psycopg2.connect(database="dsidb", user="postgres", password="developer#!", host="121.129.214.6", port="8432")
+conn = psycopg2.connect(database="dsidb", user="postgres", password="developer#!", host="121.129.214.6", port="8432")
+
+print(conn)
