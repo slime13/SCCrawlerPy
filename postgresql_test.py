d261961ebfa7ed2cfed499c6c9c3c568b664d970
Wonwoo Ro <wonwoo.ro2@gmail.com>
2020-11-02 18:46:17 +0900

    Delete postgresql_test.py

--- postgresql_test.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-print('psycopg2 install test.')
-
-conn = psycopg2.connect(database="dsidb", user="postgres", password="developer#!", host="121.129.214.6", port="8432")
-
-print(conn)
-
-conn.close()
