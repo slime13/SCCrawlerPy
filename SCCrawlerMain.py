7585baf7d6afa2df7f69d2c2294ece75f4abad1a
Wonwoo Ro <wonwoo.ro2@gmail.com>
2020-11-03 18:01:10 +0900

    Update SCCrawlerMain.py

--- SCCrawlerMain.py
+++ SCCrawlerMain.py
@@ -1,9 +1,12 @@
+import sys
+
+reload(sys) 
+sys.setdefaultencoding('utf-8')
+
 import SCCrawler
 import time
 import re
 import threading
-import sys
-
 """
 
 """
@@ -18,8 +21,7 @@ def threadStarter():
 	print('Crawling done. (%s)' % time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
 
 if __name__ == '__main__':
-	reload(sys) 
-	sys.setdefaultencoding('utf-8')
+
 	configFile = open('config.ini', 'r', encoding='utf-8')
 	lines = configFile.readlines()
 
